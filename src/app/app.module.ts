bd182a0e55945feadb505c2ecd32987709bae0d0
harynyainarajaonah01@gmail.com <harynyainarajaonah01@gmail.com>
2022-04-03 14:49:56 +0200

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,34 @@ import {HttpClientModule} from '@angular/common/http';
 import { LoginComponent } from './pages/Authentification/login/login.component';
 import { InscriptionComponent } from './pages/Authentification/inscription/inscription.component';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './pages/home/home.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    InscriptionComponent
+    InscriptionComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
