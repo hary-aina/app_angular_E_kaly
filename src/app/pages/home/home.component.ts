cb3151c696f22541b9334c850751c20c9ea277c0
harynyainarajaonah01@gmail.com <harynyainarajaonah01@gmail.com>
2022-04-04 06:08:04 +0200

    working on home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
+import { ClientService } from 'src/app/services/client.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  per_page = 10;
+  page_number = 1;
+  plats = [];
+
+  error = "";
+
+  constructor(
+    private clientService : ClientService,
+    private cookie : CookieService,
+    private router : Router
+  ) { }
 
   ngOnInit(): void {
+    this.getPlat();
+  }
+
+  getPlat(){
+    let result = this.clientService.getPlat(this.per_page, this.page_number);
+      result.subscribe((data:any) => {
+        //console.log(data);
+        if(data.status != 200){
+          this.error = "erreur lors du cosulation du serveur";
+        }else{
+          //use cookie there
+          this.plats = data;
+        }
+      });
   }
 
 }
