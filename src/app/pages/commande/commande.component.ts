06d2700c385c0728786f2885e2c9607f22696298
harynyainarajaonah01@gmail.com <harynyainarajaonah01@gmail.com>
2022-04-05 09:43:25 +0200

    faire fonctionner le l'insert commande et update

--- src/app/pages/commande/commande.component.ts
+++ src/app/pages/commande/commande.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
+import { ClientService } from 'src/app/services/client.service';
 
 @Component({
   selector: 'app-commande',
@@ -7,9 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommandeComponent implements OnInit {
 
-  constructor() { }
+  MesCommandes : any = [];
+
+  Client_name : string;
+  Client_id : string;
+  Client_contact : string;
+  token : string;
+
+  //pagination du menu
+  per_page = 10;
+  page_number = 1;
+
+  constructor(
+    private clientService : ClientService,
+    private cookie : CookieService,
+    private router : Router
+  ) { 
+    this.Client_name = this.cookie.get('user_name');
+    this.Client_id = this.cookie.get('user_id');
+    this.Client_contact = this.cookie.get('user_contact');
+    this.token = this.cookie.get('token');
+  }
 
   ngOnInit(): void {
+    this.getMyCommande();
+  }
+
+  //avoir mes commandes
+  getMyCommande(){
+    let result = this.clientService.getCommande(this.token, this.Client_id, this.per_page, this.page_number);
+      result.subscribe((data:any) => {
+        if(data.status != 200){
+          alert("erreur lors du cosulation du serveur");
+        }else{
+          //use cookie there
+          this.MesCommandes = data.data;
+          //console.log(this.MesCommandes);
+        }
+      });
+  }
+
+  //voir detail
+  getDetailCommande(macommande:any){
+    this.router.navigate(['/detail_commande'], {state:{action:'update', commande:macommande}});
   }
 
 }
--- src/app/pages/detail-commande/detail-commande.component.ts
+++ src/app/pages/detail-commande/detail-commande.component.ts
@@ -10,6 +10,8 @@ import { ClientService } from 'src/app/services/client.service';
 })
 export class DetailCommandeComponent implements OnInit {
 
+  token : string;
+
   action : any;
   extraPlat : any = [];
   extraCommande : any = [];
@@ -33,6 +35,7 @@ export class DetailCommandeComponent implements OnInit {
     this.Client_name = this.cookie.get('user_name');
     this.Client_id = this.cookie.get('user_id');
     this.Client_contact = this.cookie.get('user_contact');
+    this.token = this.cookie.get('token');
    }
 
   ngOnInit(): void {
@@ -43,6 +46,8 @@ export class DetailCommandeComponent implements OnInit {
     if(this.action == 'insert'){
       this.extraPlat = history.state.plat;
 
+      let now = new Date();
+
       this.CommandeObject = {
         restaurant_id: this.extraPlat.restaurant_id,
         restaurant_name: this.extraPlat.restaurant_name,
@@ -50,16 +55,16 @@ export class DetailCommandeComponent implements OnInit {
         client_id: this.Client_id,
         client_name: this.Client_name,
         client_contact: this.Client_contact,
-        date_comande: new Date,
+        date_comande: now.toISOString().split('T')[0]+" "+now.toISOString().split('T')[1],
         lieu_adresse_livraison: "",
         livreur_id: "",
         livreur_name: "",
         detail_commande : [
             {
-                plat_id: this.extraPlat._id,
-                plat_name: this.extraPlat.name,
-                plat_prix: this.extraPlat.prix,
-                nombre: 1
+              plat_id: this.extraPlat._id,
+              plat_name: this.extraPlat.name,
+              plat_prix: this.extraPlat.prix,
+              nombre: 1
             }
         ],
         etat: 0 
@@ -74,11 +79,32 @@ export class DetailCommandeComponent implements OnInit {
     if(this.action == 'update'){
       this.extraCommande = history.state.commande;
       //console.log(this.extraCommande);
+
+      this.CommandeObject = {
+        _id : this.extraCommande._id,
+        restaurant_id: this.extraCommande.restaurant_id,
+        restaurant_name: this.extraCommande.restaurant_name,
+        prix_global: this.extraCommande.prix_global,
+        client_id: this.extraCommande.client_id,
+        client_name: this.extraCommande.client_name,
+        client_contact: this.extraCommande.client_contact,
+        date_comande: this.extraCommande.date_comande,
+        lieu_adresse_livraison: this.extraCommande.lieu_adresse_livraison,
+        livreur_id: "",
+        livreur_name: "",
+        detail_commande : this.extraCommande.detail_commande,
+        etat: 0 
+      }
+
+      //avoir menu du restaurant
+      this.getPlatResto(this.extraCommande.restaurant_id);
+
     }
   }
 
+  //avoir plat d'un restaurant donnée
   getPlatResto(resto_id : string){
-    alert("tafiditra");
+    //alert("tafiditra");
     let result = this.clientService.getPlatByRestaurant(resto_id, this.per_page, this.page_number);
       result.subscribe((data:any) => {
         if(data.status != 200){
@@ -90,5 +116,119 @@ export class DetailCommandeComponent implements OnInit {
         }
       });
   }
+
+  //|--------------------|
+  //|  gestion des plat  |
+  //|--------------------|
+
+  //addPlat in list
+  addPlat(plat:any){
+    let tmpPlat = {
+      plat_id: plat._id,
+      plat_name: plat.name,
+      plat_prix: plat.prix,
+      nombre: 1
+    }
+    this.CommandeObject.prix_global = parseFloat(this.CommandeObject.prix_global) + parseFloat(plat.prix);
+    this.CommandeObject.detail_commande.push(tmpPlat);
+
+    console.log(this.CommandeObject, "add Plat");
+    
+  }
+
+  plusPlatNumber(plat:any){
+    plat.nombre++;
+    this.CommandeObject.prix_global = parseFloat(this.CommandeObject.prix_global) + parseFloat(plat.plat_prix);
+    //console.log(this.CommandeObject, "+ Plat");
+  }
+
+  minusPlatNumber(plat:any){
+    plat.nombre --;
+    this.CommandeObject.prix_global = parseFloat(this.CommandeObject.prix_global) - parseFloat(plat.plat_prix);
+    if(plat.nombre == 0){
+      this.removePlat(plat);
+    }
+  }
+
+  //removePlat in list
+  removePlat(plat:any){
+    this.CommandeObject.detail_commande = this.CommandeObject.detail_commande.filter((item: any) => item != plat);
+    this.CommandeObject.prix_global = parseFloat(this.CommandeObject.prix_global) - (parseFloat(plat.plat_prix)*parseInt(plat.nombre));
+    
+  }
+
+  //|-----------------------|
+  //| fin gestion des plat  |
+  //|-----------------------|
+
+
+  //|-------------------------|
+  //| gestion de la commande  |
+  //|-------------------------|
+  
+  //inserer la commande
+  makeOrder(){
+    let result = this.clientService.makeOrdre(
+        this.token, 
+        this.CommandeObject.restaurant_id, 
+        this.CommandeObject.restaurant_name, 
+        this.CommandeObject.prix_global, 
+        this.CommandeObject.client_id, 
+        this.CommandeObject.client_name, 
+        this.CommandeObject.client_contact, 
+        this.CommandeObject.date_comande, 
+        this.CommandeObject.lieu_adresse_livraison, 
+        this.CommandeObject.detail_commande
+      );
+    result.subscribe((data:any) => {
+      //console.log(data);
+      if(data.status != 200){
+        alert("lors de l'ajout de la commande");
+      }else{
+        //use cookie there
+        //console.log(data);
+        this.router.navigate(['/commande']);
+      }
+    });
+  }
+
+  //modifier commande
+  updateOrder(){
+    let result = this.clientService.updateOrdre(
+        this.token, 
+        this.CommandeObject._id, 
+        this.CommandeObject.prix_global, 
+        this.CommandeObject.detail_commande, 
+        this.CommandeObject.lieu_adresse_livraison, 
+        this.CommandeObject.client_contact
+      );
+    result.subscribe((data:any) => {
+      //console.log(data);
+      if(data.status != 200){
+        alert("lors de la mise a jour de la commande");
+      }else{
+        //use cookie there
+        //console.log(data);
+        this.router.navigate(['/commande']);
+      }
+    });
+  }
+
+  //determinate action
+  actionOrder(){
+    if(this.action == "insert"){
+      this.makeOrder();
+    }
+    else if(this.action == "update"){
+      this.updateOrder();
+    }
+    else{
+      alert("action non defini");
+    }
+  }
+
+  //|-----------------------------|
+  //| fin gestion de la commande  |
+  //|-----------------------------|
 
 }
